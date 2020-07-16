fa7b1d2b7a4472f731b83f3f7ae7b81ad6c3dd38
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-07-16 14:09:28 +0530

    collapse location

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -3,10 +3,12 @@ import Map from "./Map";
 
 const OrderItem = (props) => (
     <div className="App-orders-item">
+        <a href="#">
+            <img src={props.src_img} className="App-orders-item-img" alt="logo"/>
+        </a>
+
         <div>
-            <a href="#">
-                <img src={props.src_img} className="App-orders-item-img" alt="logo"/>
-            </a>
+            {/*<a href={`#` + props.uidx} data-toggle="collapse">{props.location.address.substring(0, 20)}...</a>*/}
             <a href={`#` + props.uidx} data-toggle="collapse">location</a>
         </div>
         <div id={props.uidx} className="collapse">
