afbc9323d6cbb036751add3ba4be4667d6b9682a
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-07-16 21:22:15 +0530

    image updated

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import crocs from "../images/crocs.png";
 import OrderItem from "./OrderItem";
 import rawDataJson from "../data/data.json"
 
@@ -7,7 +6,7 @@ const OrderDetails = () => (
     <div className="App-orders-items">
         {
             rawDataJson.orders.map((data) => {
-                return <OrderItem uidx={data.uidx} src_img={crocs} location={data.location}/>
+                return <OrderItem uidx={data.uidx} src_img={data.orderInfo} location={data.location}/>
             })
         }
     </div>
--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Map from "./Map";
+import crocs from "../images/earphones.jpeg";
+
 
 const OrderItem = (props) => (
     <div className="App-orders-item">
         <a href="#">
-            <img src={props.src_img} className="App-orders-item-img" alt="logo"/>
+            <img src={crocs} className="App-orders-item-img" alt="logo"/>
             {/*<img src={require('../images/' + props.src_img + '.png')} className="App-orders-item-img" alt="logo"/>*/}
         </a>
 
         <div>
             {/*<a href={`#` + props.uidx} data-toggle="collapse">{props.location.address.substring(0, 20)}...</a>*/}
-            <a href={`#` + props.uidx} data-toggle="collapse">location</a>
+            <a href={`#` + props.uidx} data-toggle="collapse">Track order location</a>
         </div>
         <div id={props.uidx} className="collapse">
             <Map location={props.location} zoomLevel={15}/>
